1f44a33d481317bee101c697d5a000e22c3be464
Fernando Morales H <f.m199698@gmail.com>
2022-05-20 21:19:00 -0500

    Agregando 1er Requerimiento por TDD

--- weekly_mission_3/my_launchx_app/index.js
+++ weekly_mission_3/my_launchx_app/index.js
@@ -2,6 +2,6 @@ const MissionCommander = require('./app/missionCommander');
 const carlo = new MissionCommander("Carlo", 22, "Javascript")
 const fer = new MissionCommander ("Fernanda Ochoa")
 const rodrigo = new MissionCommander ("Rodrigo")
-console.log("Nombre MC BackEnd: "+ carlo.name/*, carlo.age, carlo.program*/)
+console.log("Nombre MC BackEnd: "+ carlo.name + carlo.age + carlo.program)
 console.log("Nombre MC Onboarding: " + fer.name)
 console.log("Nombre MC FrontEnd: " + rodrigo.name)
