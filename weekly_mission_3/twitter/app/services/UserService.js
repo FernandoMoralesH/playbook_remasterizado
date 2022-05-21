723dc83a01699e16f135d3ac5bb2929dd1704527
Fernando Morales H <f.m199698@gmail.com>
2022-05-21 18:26:50 -0500

    Agregando pruebas a clases en services

---
+++ weekly_mission_3/twitter/app/services/UserService.js
@@ -0,0 +1,13 @@
+const User = require ("./../models/user")
+
+class UserService {
+    static create(id, username, name){
+        return new User (id, username, name, "Sin bio")
+    }
+    static getInfo (user){
+        return [user.id, user.username, user.name, user.bio]
+    }
+    }
+
+
+module.exports = UserService
